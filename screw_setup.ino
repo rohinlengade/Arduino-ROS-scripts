86129f5d37933ee7d1ada245019b2e4c67b7d423
Rohin Lengade <54680230+rohinlengade@users.noreply.github.com>
2023-10-18 10:32:09 -0700

    Screw_setup.ino

--- screw_setup.ino
+++ screw_setup.ino
@@ -28,18 +28,19 @@ ros::NodeHandle nh;
 
 //Publisher
 std_msgs::String ActState;
-ros::Publisher ActuatorState("state", &ActState);
+ros::Publisher ActuatorSate("state", &ActState);
 
-char sm0[10] = "0 degrees";
-char sm30[11] = "30 degrees";
+//char sm0[10] = "0 degrees";
+char bg30[11] = "30 degrees";
 char bg60[11] = "60 degrees";
 char bg90[11] = "90 degrees";
 char bg120[12] = "120 degrees";
 
 //callback function
 
+//Small Actuator
 //Zero degrees
-void s0(const std_msgs::Empty& toggle_msg) {
+/*void s0(const std_msgs::empty& toggle_msg) {
   digitalWrite(rpin1, LOW);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, LOW);
@@ -50,14 +51,28 @@ void s0(const std_msgs::Empty& toggle_msg) {
   digitalWrite(rpin3, HIGH);
   digitalWrite(rpin4, HIGH);
   ActState.data = sm0;
-}
+}*/
 //Thirty degrees
-void s30(const std_msgs::Empty& toggle_msg) {
+void b30(const std_msgs::empty& toggle_msg) {
   digitalWrite(rpin1, LOW);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, LOW);
   digitalWrite(rpin4, HIGH);
-  delay(6000);
+  delay(14000);
+  digitalWrite(rpin1, HIGH);
+  digitalWrite(rpin2, HIGH);
+  digitalWrite(rpin3, HIGH);
+  digitalWrite(rpin4, HIGH);
+  ActState.data = bg30;
+}
+//Big Actuator
+//Sixty degrees
+void b60(const std_msgs::empty& toggle_msg) {
+  digitalWrite(rpin1, LOW);
+  digitalWrite(rpin2, HIGH);
+  digitalWrite(rpin3, LOW);
+  digitalWrite(rpin4, HIGH);
+  delay(14000);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
@@ -67,34 +82,20 @@ void s30(const std_msgs::Empty& toggle_msg) {
   digitalWrite(rpin2, LOW);
   digitalWrite(rpin3, HIGH);
   digitalWrite(rpin4, LOW);
-  delay(2000);
+  delay(3500);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
   digitalWrite(rpin4, HIGH);
-  ActState.data = sm30;
-}
-//Sixty degrees
-void b60(const std_msgs::Empty& toggle_msg) {
-  digitalWrite(rpin1, LOW);
-  digitalWrite(rpin2, HIGH);
-  digitalWrite(rpin3, LOW);
-  digitalWrite(rpin4, HIGH);
-  delay(6000);
-  digitalWrite(rpin1, HIGH);
-  digitalWrite(rpin2, HIGH);
-  digitalWrite(rpin3, HIGH);
-  digitalWrite(rpin4, HIGH);
-  delay(500);
   ActState.data = bg60;
 }
 //Ninety degrees
-void b90(const std_msgs::Empty& toggle_msg) {
+void b90(const std_msgs::empty& toggle_msg) {
   digitalWrite(rpin1, LOW);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, LOW);
   digitalWrite(rpin4, HIGH);
-  delay(6000);
+  delay(14000);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
@@ -104,7 +105,7 @@ void b90(const std_msgs::Empty& toggle_msg) {
   digitalWrite(rpin2, LOW);
   digitalWrite(rpin3, HIGH);
   digitalWrite(rpin4, LOW);
-  delay(2000);
+  delay(9400);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
@@ -112,12 +113,12 @@ void b90(const std_msgs::Empty& toggle_msg) {
   ActState.data = bg90;
 }
 //One-Twenty degrees
-void b120(const std_msgs::Empty& toggle_msg) {
+void b120(const std_msgs::empty& toggle_msg) {
   digitalWrite(rpin1, LOW);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, LOW);
   digitalWrite(rpin4, HIGH);
-  delay(6000);
+  delay(14000);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
@@ -127,7 +128,7 @@ void b120(const std_msgs::Empty& toggle_msg) {
   digitalWrite(rpin2, LOW);
   digitalWrite(rpin3, HIGH);
   digitalWrite(rpin4, LOW);
-  delay(4000);
+  delay(13500);
   digitalWrite(rpin1, HIGH);
   digitalWrite(rpin2, HIGH);
   digitalWrite(rpin3, HIGH);
@@ -136,8 +137,8 @@ void b120(const std_msgs::Empty& toggle_msg) {
 }
 
 //subscriber
-ros::Subscriber<std_msgs::Empty>sa0("Zero degrees", &s0);
-ros::Subscriber<std_msgs::Empty>sa30("Thirty degrees", &s30);
+//ros::Subscriber<std_msgs::Empty>0("Zero degrees", &s0);
+ros::Subscriber<std_msgs::Empty>ba30("Thirty degrees", &b30);
 ros::Subscriber<std_msgs::Empty>ba60("Sixty degrees", &b60);
 ros::Subscriber<std_msgs::Empty>ba90("Ninety degrees", &b90);
 ros::Subscriber<std_msgs::Empty>ba120("One-Twenty degrees", &b120);
@@ -160,8 +161,8 @@ void setup() {
 
   //Initialize ROS stuff
   nh.initNode();
-  nh.subscribe(sa0);
-  nh.subscribe(sa30);
+  //nh.subscribe(0);
+  nh.subscribe(ba30);
   nh.subscribe(ba60);
   nh.subscribe(ba90);
   nh.subscribe(ba120);
@@ -171,7 +172,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  ActuatorState.publish( &ActState);
+  ActuatorState.pubish( &ActState);
   nh.spinOnce();
   delay(1);
 }
